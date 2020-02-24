0dbe1fd43b09086a22f26db0cbb01453ee781b62
Suraj <hrishabhsuraj52@gmail.com>
2020-02-24 12:48:29 +0530

    updated 6d pose

--- src/pose_estimator6d.cpp
+++ src/pose_estimator6d.cpp
@@ -44,7 +44,7 @@ bool sortTemplateView(std::pair<float, TemplateView*> a, std::pair<float, Templa
 }
 
 
-PoseEstimator6D::PoseEstimator6D(int width, int height, float zNear, float zFar, const cv::Matx33f &K, const cv::Matx14f &distCoeffs, vector<Object3D*> &objects)
+PoseEstimator6D::PoseEstimator6D(int width, int height, float zNear, float zFar, const cv::Matx33f &K, const cv::Mat &distCoeffs, vector<Object3D*> &objects)
 {
     renderingEngine = RenderingEngine::Instance();
     optimizationEngine = new OptimizationEngine(width, height);
